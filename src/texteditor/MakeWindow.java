ab77e9efae3a3219c60729ff52160e72268ea54d
god <god@DESKTOP-FGUFE84>
2017-01-31 23:01:26 +0900

    Testコミット

---
+++ src/texteditor/MakeWindow.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package texteditor;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+
+/**
+ *
+ * @author god
+ */
+public class MakeWindow {
+    
+    public void createWindow(){
+        
+    }
+}
+
+class Myframe extends JFrame{
+    private Container container = new Container();
+    Myframe(){
+        this("名無しのテキスト");
+        
+    }
+    Myframe(String title){
+        super(title);
+        
+    }
+            
+}
